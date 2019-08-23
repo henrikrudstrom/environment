e9909e1220bb6b138fd0edcadd141e62930096d2
Henrik Rudstrøm <henrik.rudstrom@amedia.no>
2019-08-23 20:07:56 +0200

    initial

---
+++ export-extensions.sh
@@ -0,0 +1,2 @@
+code --list-extensions | xargs -L 1 echo code --install-extension > install-extensions.sh
+chmod +x install-extensions.sh
---
+++ link.sh
@@ -0,0 +1,3 @@
+ln -h settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -h keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/Documents/GitHub/vscode-settings/snippets ~/Library/Application\ Support/Code/User/snippets
