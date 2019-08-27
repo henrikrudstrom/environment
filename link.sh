b42504f5da02f20dfc34bc4117fa325ea00dacce
Henrik Rudstrøm <henrik.rudstrom@amedia.no>
2019-08-27 13:33:20 +0200

    updated settings and link command

--- link.sh
+++ link.sh
@@ -1,3 +1,3 @@
 ln -h settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -h keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/Documents/GitHub/vscode-settings/snippets ~/Library/Application\ Support/Code/User/snippets
+ln -s `pwd`/snippets/ ~/Library/Application\ Support/Code/User/
