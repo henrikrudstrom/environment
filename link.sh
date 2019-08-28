6db19cff51a3218347ab708dfab0255c1dc627f2
Henrik Rudstrøm <henrik.rudstrom@amedia.no>
2019-08-28 12:47:34 +0200

    added tigrc to repo

--- link.sh
+++ link.sh
@@ -1,3 +1,4 @@
 ln -h settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -h keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s `pwd`/snippets/ ~/Library/Application\ Support/Code/User/
+ln -h .tigrc ~/.tigrc
