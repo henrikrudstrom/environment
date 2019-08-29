e9e4a295170b789aaee9a3c606212402d96a3db5
Henrik Rudstrøm <henrik.rudstrom@amedia.no>
2019-08-29 16:05:36 +0200

    added hammerspoon config to repo

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 ln -h settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -h keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s `pwd`/snippets/ ~/Library/Application\ Support/Code/User/
+ln -s `pwd`/.hammerspoon ~/
 ln -h .tigrc ~/.tigrc
