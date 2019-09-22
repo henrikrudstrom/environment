214db285da02c3d4478d214e42ad8096ac63c1d2
Henrik Rudstrøm <henrik.rudstrom@amedia.no>
2019-09-22 19:01:18 +0200

    added autojump

--- .zshrc.d/init.zsh
+++ .zshrc.d/init.zsh
@@ -3,3 +3,4 @@ eval "$(rbenv init -)"
 source "/Users/henrik.rudstrom/dev/amedia/tools/init.zsh"
 
 eval $(thefuck --alias fuck)
+[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
